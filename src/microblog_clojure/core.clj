86db932fc363f38f14e499411b9b63002fa2cc43
PiratePowWow <Thi3v3ry>
2016-03-24 11:12:59 -0400

    Start Server With / Route

--- src/microblog_clojure/core.clj
+++ src/microblog_clojure/core.clj
@@ -1,5 +1,15 @@
 (ns microblog-clojure.core
+  (:require [ring.adapter.jetty :as j]
+            [compojure.core :as c])
   (:gen-class))
 
+(c/defroutes app
+  (c/GET "/" request
+    "Hello World"))
+
+(defonce server (atom nil))
+
 (defn -main []
-  (println "Hello, World!"))
+  (when @server
+      (.stop @server))
+  (reset! server (j/run-jetty app {:port 3000 :join? false})))
